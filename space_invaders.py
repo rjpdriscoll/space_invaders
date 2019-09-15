b77c5a53fdf546040098318f076a79401c3b7546
rjpdriscoll <rjpdriscoll@gmail.com>
2019-09-15 15:34:01 -0700

    added ship movement left and right. set boundaries for screen

--- space_invaders.py
+++ space_invaders.py
@@ -22,14 +22,40 @@ class SpaceInvaders:
         # Start the main loop for the game
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            
+
+    def _check_events(self):
+        # Respond to keypresses and mouse events
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            # Redraw the screen during each pass through the loop.
+                elif event.type == pygame.KEYDOWN:
+                    self._check_keydown_events(event)
+                elif event.type == pygame.KEYUP:
+                    self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        # Respond to key presses.
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+    
+    def _check_keyup_events(self, event):
+        # Respond to key releases.
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+    
+    def _update_screen(self):
+        # Update images on the screen, and flip to the new screen.
             self.screen.fill(self.bg_color)
             self.ship.blitme()
             
-            # Make the most recently drawn screen visible.
             pygame.display.flip()
 
 if __name__ == '__main__':
