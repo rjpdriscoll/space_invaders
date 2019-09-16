485f2cac71383bc32cb210d9afa61f4bf5f3fc86
rjpdriscoll <rjpdriscoll@gmail.com>
2019-09-15 17:58:35 -0700

    game has full fleet of respawning aliens. collision detection implemented.

--- settings.py
+++ settings.py
@@ -12,8 +12,14 @@ class Settings:
         # Ship settings
         self.ship_speed = 1.5
 
+        # Alien settings
+        self.alien_speed = 0.75
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
         # Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.25
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
